4dd287018fea860d78c71be4836b51e3735b0278
stellaxxy <stellaxxy0109@gmail.com>
2019-09-02 18:47:19 -0700

    initial push with user list page

---
+++ themes/custom/user_list/js/main.js
@@ -0,0 +1,42 @@
+
+(function($){
+    $(document).ready(
+        $('.full-name').hover(tooltipHandlerIn, tooltipHandlerOut),
+        $('.card-inner').click(flipCard),
+        arrangeAlphabetically()
+    )
+
+    function tooltipHandlerIn(){
+        let phone = $(this).data('phone');
+        let tooltipText = $('<span></span>').text(`Tel: ${phone}`).addClass('tooltip-text');
+        $(this).append(tooltipText);
+    }
+
+    function tooltipHandlerOut(){
+        $(this).find($('.tooltip-text')).remove();
+    }
+
+    function arrangeAlphabetically(){
+        let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+        for(let index = 0; index < letters.length; index++){
+            if($(`.card.${letters[index]}`).length > 0){
+                if($(`.${letters[index]}-container`).length === 0){
+                    let heading = $('<h3>').text(letters[index].toUpperCase());
+                    let container = $('<div>').addClass(`${letters[index]}-container alpha-container`).append($(heading));
+                    $(container).append($(`.card.${letters[index]}`));
+                    $('.rows-container').append($(container));
+                } else {
+                    $(`.${letters[index]}-container`).append($(`.card.${letters[index]}`));
+                }
+            }
+        }
+    }
+
+    function flipCard(){
+        $(this).toggleClass('rotate-card');
+        setTimeout(()=>{$(this).find($('.card-front')).toggleClass('hide-front')}, 200);
+    }
+
+
+})(jQuery);
